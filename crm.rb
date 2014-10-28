0c2b94fcafa6a99d7e6b7b83c076bdc4452e683a
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-28 14:25:05 -0400

    Add contacts.erb

--- crm.rb
+++ crm.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 
 get '/' do
-	@crm_app_name = "Nic's CRM"		#To be passed along to view
-	erb :index
+	@crm_app_name = "Nic's CRM"		# To be passed along to view
+	erb :index						# Links to HTML (erb) index file
+end
+
+get '/contacts' do
+	erb :contacts
 end
